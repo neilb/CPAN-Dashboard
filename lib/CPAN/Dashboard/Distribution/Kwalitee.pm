3acbb2ae8937c6aaf7c9ef3a376884cf55630634
Mohammad S Anwar <mohammad.anwar@yahoo.com>
2015-01-13 14:38:11 +0000

    - Added pod document to the following packages:   - CPAN::Dashboard::Distribution   - CPAN::Dashboard::Distribution:CPANTesters   - CPAN::Dashboard::Distribution::Kwalitee

--- lib/CPAN/Dashboard/Distribution/Kwalitee.pm
+++ lib/CPAN/Dashboard/Distribution/Kwalitee.pm
@@ -6,4 +6,27 @@ use Moo;
 has kwalitee      => (is => 'ro');
 has core_kwalitee => (is => 'ro');
 
+=head1 NAME
+
+CPAN::Dashboard::Distribution::Kwalitee - Package to manage the KWALITEE of distribution for CPAN Dashboard.
+
+=head1 DESCRIPTION
+
+B<FOR INTERNAL USE ONLY>
+
+=head1 REPOSITORY
+
+L<https://github.com/neilbowers/CPAN-Dashboard>
+
+=head1 AUTHOR
+
+Neil Bowers E<lt>neilb@cpan.orgE<gt>
+
+=head1 COPYRIGHT AND LICENSE
+
+This software is copyright (c) 2014 by Neil Bowers <neilb@cpan.org>.
+
+This is free software; you can redistribute it and/or modify it under
+the same terms as the Perl 5 programming language system itself.
+
 1;
